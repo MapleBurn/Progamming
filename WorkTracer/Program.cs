ca32a2d0b97c0766b8d6cd731f3df40f13f6f39e
MapleBurn <miki.holan@outlook.cz>
2025-05-22 14:45:35 +0200

    ok

--- WorkTracer/Program.cs
+++ WorkTracer/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddSingleton<AuthService>();
+builder.Services.AddScoped<AuthService>();
 
 var app = builder.Build();
 
