c59c3ce307303493c3d685149c5c63216bedc243
MapleBurn <miki.holan@outlook.cz>
2025-05-22 13:12:00 +0200

    fififiifififif

--- WorkTracer/Data/PlannerEvent.cs
+++ WorkTracer/Data/PlannerEvent.cs
@@ -3,12 +3,6 @@ public class PlannerEvent
     public string Id { get; set; }
     public string Name { get; set; }
     public List<(int col, int hour)> Cells { get; set; } = new List<(int col, int hour)>();
-<<<<<<< Updated upstream
-    public int Column { get; set; }
-    public int StartHour { get; set; }
-    public int Duration { get; set; }
-=======
     public string Text { get; set; }
     public enum Type {Working, Break, Overtime, Absence} 
->>>>>>> Stashed changes
 }
