b5ef0c4b381f8ce5f1c00e28d961fec9b434bdb8
MapleBurn <miki.holan@outlook.cz>
2025-05-22 13:30:25 +0200

    pokus (bude fungovat)

--- WorkTracer/Program.cs
+++ WorkTracer/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddSignleton<AuthService>();
+builder.Services.AddSingleton<AuthService>();
 
 var app = builder.Build();
 
