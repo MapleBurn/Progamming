cb9285f302b92017dca73ca566bdc40ad20fda2d
LukoRaptor <lukas.cikan@gmail.com>
2025-05-23 15:20:49 +0200

    Planner improvements - major

--- WorkTracer/Data/PlannerEvent.cs
+++ WorkTracer/Data/PlannerEvent.cs
@@ -1,8 +1,9 @@
+using EnvironmentName = Microsoft.AspNetCore.Hosting.EnvironmentName;
+
 public class PlannerEvent
 {
     public string Id { get; set; }
     public string Name { get; set; }
     public List<(int col, int hour)> Cells { get; set; } = new List<(int col, int hour)>();
     public string Text { get; set; }
-    public enum Type {Working, Break, Overtime, Absence} 
 }
