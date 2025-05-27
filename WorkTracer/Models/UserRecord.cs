049afaea029aca85bf233a5ffe11108bb1374367
MapleBurn <miki.holan@outlook.cz>
2025-05-27 19:56:40 +0200

    DB fix2

--- WorkTracer/Models/UserRecord.cs
+++ WorkTracer/Models/UserRecord.cs
@@ -4,40 +4,9 @@ using System.Text.Json.Serialization;
 
 public class UserRecord
 {
+    public int Id { get; set; }
     public string Username {get; set;}
     public string Password {get; set;}
-    private static readonly List<UserRecord> users;
-
-    static UserRecord()
-    {
-        var json = "{}";
-        // adjust the path as needed
-        var path = "../WorkTracer/wwwroot/resources/users.json";
-        try
-        {
-            json = File.ReadAllText(path);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            using var sW = File.CreateText(path);
-            sW.WriteLine("{}");
-        }
-
-        users = JsonSerializer.Deserialize<List<UserRecord>>(json) ?? new List<UserRecord>();
-    }
-
-    public UserRecord()
-    {
-    }
-
-    public Task<bool> ValidateCredentialsAsync(string userName, string password)
-    {
-        // simple plain-text match
-        if (users.Any(u => u.Username == userName && u.Password == password))
-        {
-            return Task.FromResult(true);
-        }
-        return Task.FromResult(false);
-    }
+    public string Email {get; set;}
+    
 }
